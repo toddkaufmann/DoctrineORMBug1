daeafa578faf933c7b465a6cad1e238458ba86a2
Todd Kaufmann <toddkaufmann+github@gmail.com>
2015-08-12 19:30:40 -0400

    about to make some changes.

--- doctrine-declare-only-once.sql
+++ doctrine-declare-only-once.sql
@@ -1,5 +1,12 @@
+  drop table c1_table;
   drop table c2_table;
+
+  drop table tab1;
+
   drop table tab2;
+
+  drop table tab3;
+
 
 -- very simple: a table with id and a value
   create table c1_table ( id int not null auto_increment, v1 int, primary key (id)) engine=innodb;
@@ -12,39 +19,38 @@
     primary key (c1,c2_id), 
     constraint fk_c2 foreign key (c2_id) REFERENCES `c2_table`(id) 
   ) engine=innodb;
-  
 
-  create table if not exists tab2 (  
-    c1_id int not null, c2_id int not null, 
-    primary key (c1_id,c2_id), 
-    constraint fk_c1 foreign key (c1_id) REFERENCES `c1_table`(id),
-    constraint fk_c2_2 foreign key (c2_id) REFERENCES `c2_table`(id)
-  ) engine=innodb;
+xxx above note used  
 
 -- forgot had no index there; try with
 
   drop table tab2;
-
   create table tab2 (  
     c1_id int not null, c2_id int not null, 
+ abccd_value int,
     primary key (c1_id,c2_id), 
-    constraint fk_c1 foreign key (c1_id) REFERENCES `c1_table`(id),
     INDEX c1_idx (c1_id),
     INDEX c2_idx (c2_id),
-  --  constraint fk_c2 foreign key (c2_id) REFERENCES `c2_table`(id),
-  -- 'fk_c2' is already used as a name above, so we have to change
+    constraint fk_c1_2 foreign key (c1_id) REFERENCES `c1_table`(id),
     constraint fk_c2_2 foreign key (c2_id) REFERENCES `c2_table`(id)
   ) engine=innodb;
 
   -- make another similar ?
 
-  drop table tab3;
-  create table tab3 (  
+  drop table tab1;
+  create table tab1 (  
     c1_id int not null, c2_id int not null, 
+ xyzzy_value int,
     primary key (c1_id,c2_id), 
     INDEX c1_3_idx (c1_id),
     INDEX c2_3_idx (c2_id),
 -- comment out either line below to make it go away!
-    constraint fk_c1_3 foreign key (c1_id) REFERENCES `c1_table`(id),
+--    constraint fk_c1_3 foreign key (c1_id) REFERENCES `c1_table`(id),
     constraint fk_c2_3 foreign key (c2_id) REFERENCES `c2_table`(id)
   ) engine=innodb;
+
+-- hey, 
+-- tab1 tab2 are similar tables, but no code gen'd for 1 ?
+
+-- then run
+-- php app/console       doctrine:mapping:import               AppBundle annotation
